a6fc173ffa48596ab7988255ea0de04b5b0cfc9c
duckquy <duckquy@gmail.com>
2020-07-04 10:23:06 -0400

    Added footer with social handles

---
+++ src/components/footer.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import { rhythm } from '../utils/typography';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer
+        style={{
+          marginTop: rhythm(2.5),
+          paddingTop: rhythm(1),
+        }}
+      >
+        <a
+          href="https://mobile.twitter.com/duckcodes"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          twitter
+        </a>{' '}
+        &bull;{' '}
+        <a
+          href="https://github.com/duckduckquy"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          github
+        </a>{' '}
+        &bull;{' '}
+        <a
+          href="https://youtube.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          youtube
+        </a>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 
 import Bio from "../components/bio"
+import Footer from "../components/footer"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
@@ -12,7 +13,7 @@ const BlogIndex = ({ data, location }) => {
 
   return (
     <Layout location={location} title={siteTitle}>
-      <SEO title="All posts" />
+      <SEO title="" />
       <Bio />
       {posts.map(({ node }) => {
         const title = node.frontmatter.title || node.fields.slug
@@ -40,6 +41,7 @@ const BlogIndex = ({ data, location }) => {
           </article>
         )
       })}
+      <Footer />
     </Layout>
   )
 }
